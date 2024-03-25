645e4b880f3f08f09813f474fb4be02cf32371e9
Danny Calise <32149505+hollywoodchase@users.noreply.github.com>
2024-03-25 12:02:53 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,45 @@
+// <?php
+// $message = "";
 
+// if (isset($_POST['submitbtn'])) {
+//   $DBConnect = mysqli_connect("127.0.0.1", "hollywoodchase", "bigNsmall517!", "dcalise3");
+
+//   if ($DBConnect == false) {
+//     print("Unable to connect tp the database: " . mysqli_errno());
+//   } else {
+//     $TableName = "Users";
+//     $message = "Success! Your profile has been saved.<br>You entered: ";
+
+//     $firstname = stripslashes($_POST['firstname']);
+//     $lastname = stripslashes($_POST['lastname']);
+//     $age = stripslashes($_POST['age']);
+//     $favoriteplayer = stripslashes($_POST['favoriteplayer']);
+//     $favoritetournament = stripslashes($_POST['favoritetournament']);
+//     $favoritetennisbrand = stripslashes($_POST['favoritetennisbrand']);
+
+//     $SQLString = "insert into $TableName(firstname, lastname, age, favoriteplayer, favoritetournament, favoritetennisbrand) 
+//     values('$firstname', '$lastname', '$age', '$favoriteplayer', '$favoritetournament', '$favoritetennisbrand')";
+
+//     mysqli_query($DBConnect, $SQLString);
+
+//     mysqli_close($DBConnect);
+//   }
+
+// }
+// ?>
+<!DOCTYPE html>
+<html>
+
+<head>
+  <link rel="stylesheet" type="text/css" href="css/style.css" />
+  <title>Spotify Test</title>
+  <script>
+  </script>
+</head>
+
+<body>
+
+</body>
+
+
+</html>
